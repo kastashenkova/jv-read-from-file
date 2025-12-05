e891828e93f0c30883a92edf486b5c68e76bb55e
kastashenkova <astashenkova.katya@gmail.com>
2025-12-05 16:58:34 +0200

    fixed logic

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -8,15 +8,22 @@ import java.util.Arrays;
 
 public class FileWork {
     public String[] readFromFile(String fileName) throws FileNotFoundException {
-        String text;
+        StringBuilder text = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            text = bufferedReader.readLine();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                text.append(line).append(" ");
+            }
         } catch (FileNotFoundException e) {
             throw e;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String[] arr = text.split("\\W+");
+
+        if (text.length() == 0) {
+            return new String[0];
+        }
+        String[] arr = text.toString().split("\\W+");
         String[] res = {};
         for (int i = 1; i < arr.length; i++) {
             if (arr[i].startsWith("w")) {
