1801b5b34490abbde4f40f76fe06bd01b13fa54f
kastashenkova <astashenkova.katya@gmail.com>
2025-12-05 16:41:26 +0200

    fixed exception

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class FileWork {
-    private static final String PUNCTUATION = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-
     public String[] readFromFile(String fileName) throws FileNotFoundException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String text = null;
-        try {
-            text = bufferedReader.readLine();
+        String text;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            try {
+                text = bufferedReader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
